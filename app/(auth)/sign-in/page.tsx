f70c64a6afe041966cf9fc0675a3ab31ea6b9ff8
dimashikalov <dimashikalov86@mail.ru>
2025-06-19 16:14:22 +0300

    created AuthForm + auth routes

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,17 @@
+'use client';
+import AuthForm from '@/components/AuthForm';
+import { signInSchema } from '@/lib/validations';
 import React from 'react';
 
 const Page = () => {
-  return <div>Login</div>;
+  return (
+    <AuthForm
+      type="SIGN_IN"
+      schema={signInSchema}
+      defaultValues={{ email: '', password: '' }}
+      onSubmit={() => {}}
+    />
+  );
 };
 
 export default Page;
