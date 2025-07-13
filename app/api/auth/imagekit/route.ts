5f5cecb2e39c89ef420cdb57886535e508c6fbfe
dimashikalov <dimashikalov86@mail.ru>
2025-07-13 13:52:20 +0300

    test added cors-header

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -14,6 +14,30 @@ const imagekit = new ImageKit({
   urlEndpoint,
 });
 
+// export async function GET() {
+//   return NextResponse.json(imagekit.getAuthenticationParameters());
+// }
+
 export async function GET() {
-  return NextResponse.json(imagekit.getAuthenticationParameters());
+  const authParams = imagekit.getAuthenticationParameters();
+
+  return new NextResponse(JSON.stringify(authParams), {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // или конкретный фронтенд-домен
+      'Access-Control-Allow-Methods': 'GET,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // или конкретный фронтенд-домен
+      'Access-Control-Allow-Methods': 'GET,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
 }
